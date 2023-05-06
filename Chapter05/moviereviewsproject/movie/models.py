c01f50b0959cc9de8b153b515e5e4eebd4421fde
David Janssens <david.jdv@gmail.com>
2023-05-07 00:35:00 +0200

    Add comments to Chap 5

--- Chapter05/moviereviewsproject/movie/models.py
+++ Chapter05/moviereviewsproject/movie/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# Each model is a class that extends django.db.models.Model
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
